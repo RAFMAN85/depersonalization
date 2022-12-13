e4872acdc42adde1a9a8d8e06b31e2ded89b6c43
RAFMAN85 <67267712+RAFMAN85@users.noreply.github.com>
2022-12-13 16:38:44 +0300

    hash added

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <map>
+#include <cmath>
+#include "sha256.h"
 
 int main() {
-    std::string filename="C:/Users/Asus/Desktop/athlete_events.csv";
-    std::ifstream work_file(filename);
+
+    std::string filename_input="C:/Users/Asus/Desktop/athlete_events.csv";
+    std::ifstream work_file(filename_input);
     std::string line;
     char delimiter=',';
     getline(work_file,line);
     while(getline(work_file,line)){
+
         std::stringstream st(line);
         std::string id_pers, name_pers, sex_pers,age_pers, height_pers, weights_pers, country_pers, city_pers;
         getline(st,id_pers,delimiter);
@@ -19,14 +24,12 @@ int main() {
         getline(st,age_pers,delimiter);
         getline(st,height_pers,delimiter);
         getline(st,weights_pers,delimiter);
-        getline(st,country_pers,delimiter);
+        getline(st, country_pers,delimiter);
         getline(st,city_pers,delimiter);
-        std::cout<<"country: "<<country_pers<<std::endl;
+        std::cout<<"country: "<<sha256(country_pers)<<std::endl;
 
     }
     work_file.close();
-
-
 
 
     return 0;
